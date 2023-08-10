6a91d62ad771b1bbd5e3bbf3242400e0185e83d6
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-08-10 15:21:36 -0400

    build: add compiler options for testing purposes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  compiler: {
+    reactRemoveProperties: process.env.NODE_ENV === "production",
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
