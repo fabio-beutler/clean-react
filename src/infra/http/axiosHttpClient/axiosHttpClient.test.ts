fb03d7c736d7c53a093915046a36f91d0041d801
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-08-09 22:15:11 -0400

    refactor: move SUT creation to a factory method

--- src/infra/http/axiosHttpClient/axiosHttpClient.test.ts
+++ src/infra/http/axiosHttpClient/axiosHttpClient.test.ts
@@ -7,10 +7,14 @@ import { AxiosHttpClient } from "@/infra/http/axiosHttpClient/axiosHttpClient";
 vi.mock("axios");
 const mockedAxios = axios as Mocked<typeof axios>;
 
+const makeSut = (): AxiosHttpClient => {
+  return new AxiosHttpClient();
+};
+
 describe("AxiosHttpClient", () => {
   test("Should call axios with correct URL", async () => {
     const url = faker.internet.url();
-    const sut = new AxiosHttpClient();
+    const sut = makeSut();
     await sut.post({ url });
     expect(mockedAxios).toHaveBeenCalledWith(url);
   });
