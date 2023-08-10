14c8fea63fced901837aa7ac589c8fc5dad892d5
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-08-10 17:38:51 -0400

    test: ensure Login render with inputs with required initial state

--- src/presentation/pages/login/login.test.tsx
+++ src/presentation/pages/login/login.test.tsx
@@ -6,9 +6,15 @@ import Login from "./Login";
 describe("Login Component", () => {
   test("Should start with initial state", () => {
     const { getByTestId } = render(<Login />);
-    const errorWrap = getByTestId("error-wrap");
+    const errorWrap = getByTestId("error-wrap") as HTMLDivElement;
     expect(errorWrap.childElementCount).toBe(0);
     const submitButton = getByTestId("submit") as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
+    const emailStatus = getByTestId("email-status") as HTMLSpanElement;
+    expect(emailStatus.title).toBe("Campo obrigatório");
+    expect(emailStatus.textContent).toBe("🔴");
+    const passwordStatus = getByTestId("password-status") as HTMLSpanElement;
+    expect(passwordStatus.title).toBe("Campo obrigatório");
+    expect(passwordStatus.textContent).toBe("🔴");
   });
 });
