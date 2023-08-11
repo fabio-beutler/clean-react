69f9a4a334b9fab0f386c2dacd2bd35eed9c825b
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-08-10 20:01:00 -0400

    refactor: change Login tests to use faker-js

--- src/presentation/pages/login/login.test.tsx
+++ src/presentation/pages/login/login.test.tsx
@@ -1,3 +1,4 @@
+import { faker } from "@faker-js/faker";
 import {
   cleanup,
   fireEvent,
@@ -43,16 +44,18 @@ describe("Login Component", () => {
   test("Should call Validation with correct email", () => {
     const { sut, validationSpy } = makeSut();
     const emailInput = sut.getByTestId("email");
-    fireEvent.input(emailInput, { target: { value: "any_email" } });
+    const email = faker.internet.email();
+    fireEvent.input(emailInput, { target: { value: email } });
     expect(validationSpy.fieldName).toBe("email");
-    expect(validationSpy.fieldValue).toBe("any_email");
+    expect(validationSpy.fieldValue).toBe(email);
   });
 
   test("Should call Validation with correct password", () => {
     const { sut, validationSpy } = makeSut();
     const passwordInput = sut.getByTestId("password");
-    fireEvent.input(passwordInput, { target: { value: "any_password" } });
+    const password = faker.internet.password();
+    fireEvent.input(passwordInput, { target: { value: password } });
     expect(validationSpy.fieldName).toBe("password");
-    expect(validationSpy.fieldValue).toBe("any_password");
+    expect(validationSpy.fieldValue).toBe(password);
   });
 });
