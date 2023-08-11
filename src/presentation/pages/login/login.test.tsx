208c3cc9f636b020bd8e9ca76926bf353420ef0b
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-08-10 20:11:56 -0400

    test: ensure Login present error if email is invalid

--- src/presentation/pages/login/login.test.tsx
+++ src/presentation/pages/login/login.test.tsx
@@ -18,6 +18,7 @@ type SutTypes = {
 
 const makeSut = (): SutTypes => {
   const validationSpy = new ValidationSpy();
+  validationSpy.errorMessage = faker.word.sample();
   const sut = render(<Login validation={validationSpy} />);
   return { sut, validationSpy };
 };
@@ -26,13 +27,13 @@ describe("Login Component", () => {
   afterEach(cleanup);
 
   test("Should start with initial state", () => {
-    const { sut } = makeSut();
+    const { sut, validationSpy } = makeSut();
     const errorWrap = sut.getByTestId("error-wrap") as HTMLDivElement;
     expect(errorWrap.childElementCount).toBe(0);
     const submitButton = sut.getByTestId("submit") as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
     const emailStatus = sut.getByTestId("email-status") as HTMLSpanElement;
-    expect(emailStatus.title).toBe("Campo obrigatório");
+    expect(emailStatus.title).toBe(validationSpy.errorMessage);
     expect(emailStatus.textContent).toBe("🔴");
     const passwordStatus = sut.getByTestId(
       "password-status",
@@ -57,5 +58,14 @@ describe("Login Component", () => {
     fireEvent.input(passwordInput, { target: { value: password } });
     expect(validationSpy.fieldName).toBe("password");
     expect(validationSpy.fieldValue).toBe(password);
+  });
+
+  test("Should show email error if Validation fails", () => {
+    const { sut, validationSpy } = makeSut();
+    const emailInput = sut.getByTestId("email");
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } });
+    const emailStatus = sut.getByTestId("email-status");
+    expect(emailStatus.title).toBe(validationSpy.errorMessage);
+    expect(emailStatus.textContent).toBe("🔴");
   });
 });
--- src/presentation/test/mockValidation.ts
+++ src/presentation/test/mockValidation.ts
@@ -5,7 +5,7 @@ export class ValidationSpy implements Validation {
   fieldName: string = "";
   fieldValue: string = "";
 
-  validate(fieldName: string, fieldValue: string): string | null {
+  validate(fieldName: string, fieldValue: string): string {
     this.fieldName = fieldName;
     this.fieldValue = fieldValue;
     return this.errorMessage;
