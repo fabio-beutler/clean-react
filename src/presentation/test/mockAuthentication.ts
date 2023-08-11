bf9e1a225ea9725d9c9bd7fd11bc735bca8ef610
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-08-11 00:29:26 -0400

    test: prevent Login to call Authentication more than once

--- src/presentation/test/mockAuthentication.ts
+++ src/presentation/test/mockAuthentication.ts
@@ -3,10 +3,12 @@ import { mockAccountModel } from "@/domain/test";
 import { Authentication, AuthenticationParams } from "@/domain/useCases";
 
 export class AuthenticationSpy implements Authentication {
-  account = mockAccountModel();
+  account: AccountModel = mockAccountModel();
   params: AuthenticationParams = {} as AuthenticationParams;
+  callsCount: number = 0;
   async auth(params: AuthenticationParams): Promise<AccountModel> {
     this.params = params;
+    this.callsCount++;
     return Promise.resolve(this.account);
   }
 }
