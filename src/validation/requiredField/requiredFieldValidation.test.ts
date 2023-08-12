06240147c22bbfd1a9a378682e5bdb0f9759d5bf
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-08-11 20:17:24 -0400

    test: ensure RequiredFieldValidation return falsy if field is not empty

--- src/validation/requiredField/requiredFieldValidation.test.ts
+++ src/validation/requiredField/requiredFieldValidation.test.ts
@@ -1,4 +1,5 @@
-import { Validation } from "@/presentation/protocols";
+import { faker } from "@faker-js/faker";
+
 import { RequiredFieldError } from "@/validation/errors";
 
 import { RequiredFieldValidation } from "./requiredFieldValidation";
@@ -8,5 +9,11 @@ describe("RequiredFieldValidation", () => {
     const sut = new RequiredFieldValidation("email");
     const error = sut.validate("");
     expect(error).toEqual(new RequiredFieldError());
+  });
+
+  test("Should return falsy if field is not empty", () => {
+    const sut = new RequiredFieldValidation("email");
+    const error = sut.validate(faker.word.sample());
+    expect(error).toBeFalsy();
   });
 });
