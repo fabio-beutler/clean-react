9add1cd45f3cb7fcc45f9813d77250fcb6ae5873
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-08-14 20:48:24 -0400

    refactor: move SUT to a factory creation method

--- src/infra/cache/cookiesAdapter/cookiesAdapter.test.ts
+++ src/infra/cache/cookiesAdapter/cookiesAdapter.test.ts
@@ -2,9 +2,13 @@ import { faker } from "@faker-js/faker";
 
 import { CookiesAdapter } from "./cookiesAdapter";
 
+const makeSut = (): CookiesAdapter => {
+  return new CookiesAdapter();
+};
+
 describe("CookiesAdapter", () => {
   test("Should call document.cookie with correct values", () => {
-    const sut = new CookiesAdapter();
+    const sut = makeSut();
     const key = faker.database.column();
     const value = faker.word.sample();
     sut.set(key, value);
