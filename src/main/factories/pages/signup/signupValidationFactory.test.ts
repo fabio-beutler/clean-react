45de37387f9fd357244d97b3f414950205f10f2c
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-08-22 22:07:41 -0400

    test: add Signup Composition

--- src/main/factories/pages/signup/signupValidationFactory.test.ts
+++ src/main/factories/pages/signup/signupValidationFactory.test.ts
@@ -12,7 +12,6 @@ describe("SignupValidationFactory", () => {
         ...ValidationBuilder.field("password").required().min(5).build(),
         ...ValidationBuilder.field("passwordConfirmation")
           .required()
-          .min(5)
           .sameAs("password")
           .build(),
       ]),
