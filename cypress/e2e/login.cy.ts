5772e4396157172fac770027e122056f23c7a2e9
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-08-28 04:27:47 -0400

    refactor: add cypress command

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,5 +1,13 @@
 describe("Login", () => {
-  it("should login", () => {
+  beforeEach(() => {
     cy.visit("login");
+  });
+
+  it("Should load with correct initial state", () => {
+    cy.getByTestId("email-status").should(
+      "have.attr",
+      "title",
+      "Campo obrigatório",
+    );
   });
 });
