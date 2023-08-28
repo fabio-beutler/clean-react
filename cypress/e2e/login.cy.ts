02e75afb6ea8ccf917df7bdcdabf0188ac5dd714
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-08-28 04:53:17 -0400

    test: ensure Login present error state if form is invalid

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,16 +1,35 @@
+import { faker } from "@faker-js/faker";
+
 describe("Login", () => {
   beforeEach(() => {
     cy.visit("login");
   });
 
   it("Should load with correct initial state", () => {
+    cy.getByTestId("email").should("not.have.focus").and("be.empty");
+    cy.getByTestId("password").should("not.have.focus").and("be.empty");
     cy.getByTestId("email-status")
       .should("have.attr", "title", "Campo obrigatório")
-      .should("contain.text", "🔴");
+      .and("contain.text", "🔴");
     cy.getByTestId("password-status")
       .should("have.attr", "title", "Campo obrigatório")
-      .should("contain.text", "🔴");
-    cy.getByTestId("submit").should("have.attr", "disabled");
-    cy.getByTestId("error-wrap").should("not.have.descendants");
+      .and("contain.text", "🔴");
+    cy.getByTestId("submit").should("be.disabled");
+    cy.getByTestId("error-wrap").should("be.empty");
+  });
+
+  it("Should present error state if form is invalid", () => {
+    cy.getByTestId("email").focus().type(faker.word.sample());
+    cy.getByTestId("email-status")
+      .should("have.attr", "title", "Campo inválido")
+      .and("contain.text", "🔴");
+    cy.getByTestId("password")
+      .focus()
+      .type(faker.word.sample({ length: 3 }));
+    cy.getByTestId("password-status")
+      .should("have.attr", "title", "Campo inválido")
+      .and("contain.text", "🔴");
+    cy.getByTestId("submit").should("be.disabled");
+    cy.getByTestId("error-wrap").should("be.empty");
   });
 });
