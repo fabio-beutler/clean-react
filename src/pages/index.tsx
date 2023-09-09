23ddb3bca145adeb8b371c2a3ced6e0f949bca70
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-09-09 18:29:25 -0400

    refactor: move surveyList page to home path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
+"use client";
+import { NextPage } from "next";
 import { Roboto } from "next/font/google";
 import Head from "next/head";
 
+import { PrivateRoute } from "@/presentation/components";
+import { SurveyList } from "@/presentation/pages";
+
 const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
-export default function Home() {
+const HomePage: NextPage = () => {
   return (
-    <div className={roboto.className}>
-      <Head>
-        <title>4Dev - Enquetes para programadores</title>
-        <meta name="description" content="Enquetes para programadores" />
-      </Head>
-      <h1>hello</h1>
-    </div>
+    <PrivateRoute>
+      <div className={roboto.className}>
+        <Head>
+          <title>4Dev - Survey List</title>
+          <meta name="description" content="Survey List" />
+        </Head>
+        <SurveyList />
+      </div>
+    </PrivateRoute>
   );
-}
+};
+
+export default HomePage;
