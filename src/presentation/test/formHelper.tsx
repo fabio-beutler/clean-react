97042e6d15480e4fec0b4453cb44e4cdcfef0d8a
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-09-09 20:33:03 -0400

    refactor: change asserts to use vitest-dom

--- src/presentation/test/formHelper.tsx
+++ src/presentation/test/formHelper.tsx
@@ -1,24 +1,13 @@
 import { faker } from "@faker-js/faker";
 import { fireEvent, screen } from "@testing-library/react";
 
-export const testChildCount = (fieldName: string, count: number): void => {
-  const element = screen.getByTestId(fieldName);
-  expect(element.childElementCount).toBe(count);
-};
-export const testButtonIsDisabled = (
-  fieldName: string,
-  isDisabled: boolean,
-): void => {
-  const buttonElement = screen.getByTestId(fieldName) as HTMLButtonElement;
-  expect(buttonElement.disabled).toBe(isDisabled);
-};
 export const testStatusForField = (
   fieldName: string,
   validationError?: string,
 ): void => {
   const element = screen.getByTestId(`${fieldName}-status`);
-  expect(element.title).toBe(validationError || "Tudo certo!");
-  expect(element.textContent).toBe(validationError ? "🔴" : "🟢");
+  expect(element).toHaveAttribute("title", validationError || "Tudo certo!");
+  expect(element).toHaveTextContent(validationError ? "🔴" : "🟢");
 };
 
 export const populateField = (
@@ -29,14 +18,4 @@ export const populateField = (
   fireEvent.input(inputElement, {
     target: { value },
   });
-};
-
-export const testElementExists = (fieldName: string): void => {
-  const element = screen.getByTestId(fieldName);
-  expect(element).toBeTruthy();
-};
-
-export const testElementText = (fieldName: string, text: string): void => {
-  const element = screen.getByTestId(fieldName);
-  expect(element.textContent).toBe(text);
 };
