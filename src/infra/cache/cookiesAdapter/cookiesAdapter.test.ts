33cd2e611f4c371407d1bb42b91638ea7f50febf
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-09-15 21:47:18 -0400

    test: ensure CookiesAdapter calls Cookies.remove if value is null

--- src/infra/cache/cookiesAdapter/cookiesAdapter.test.ts
+++ src/infra/cache/cookiesAdapter/cookiesAdapter.test.ts
@@ -1,4 +1,5 @@
 import { faker } from "@faker-js/faker";
+import Cookies from "js-cookie";
 
 import { CookiesAdapter } from "./cookiesAdapter";
 
@@ -11,7 +12,16 @@ describe("CookiesAdapter", () => {
     const sut = makeSut();
     const key = faker.database.column();
     const value = faker.word.sample();
+    vi.spyOn(Cookies, "set");
     sut.set(key, { value });
-    expect(document.cookie).toBe(`${key}=${JSON.stringify({ value })}`);
+    expect(Cookies.set).toHaveBeenCalledWith(key, JSON.stringify({ value }));
+  });
+
+  test("Should remove document.cookie if value is null", () => {
+    const sut = makeSut();
+    const key = faker.database.column();
+    vi.spyOn(Cookies, "remove");
+    sut.set(key, undefined);
+    expect(Cookies.remove).toHaveBeenCalledWith(key);
   });
 });
