613613918e584c0a1779d64452c5107e2cba40ff
Fabio Beutler <dev.fabiobeutler@gmail.com>
2023-09-15 21:46:52 -0400

    feat: ensure CookiesAdapter calls Cookies.remove if value is null

--- src/infra/cache/cookiesAdapter/cookiesAdapter.ts
+++ src/infra/cache/cookiesAdapter/cookiesAdapter.ts
@@ -1,7 +1,13 @@
+import Cookies from "js-cookie";
+
 import { SetStorage } from "@/data/protocols/cache";
 
 export class CookiesAdapter implements SetStorage {
-  set(key: string, value: Record<any, any>): void {
-    document.cookie = `${key}=${JSON.stringify(value)}`;
+  set(key: string, value: Record<any, any> | undefined): void {
+    if (!value) {
+      Cookies.remove(key);
+    } else {
+      Cookies.set(key, JSON.stringify(value));
+    }
   }
 }
